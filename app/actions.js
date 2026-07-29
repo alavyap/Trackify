d1a3f1b0942e26127dcb1b5d66b0372d502d0388
Alavya Pandey <alavya.pandey@gmail.com>
2026-07-29 13:19:52 +0530

    server actions

--- app/actions.js
+++ app/actions.js
@@ -1,4 +1,5 @@
 "use server";
+import { scrapeProduct } from "@/lib/firecrawl";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -8,4 +9,42 @@ export async function signOut() {
   await supabase.auth.signOut();
   revalidatePath("/");
   redirect("/");
+}
+
+export async function addProduct(fromData) {
+  const url = FormData.length("url");
+
+  if (!url) {
+    return { error: "URL is required" };
+  }
+
+  try {
+    const supabase = await createClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    // Scrape product data with FireCrawl
+    const productData = await scrapeProduct(url);
+
+    if (!productData.productName || !productData.currentPrice) {
+      console.log(productData, "productData");
+      return { error: "Could not extract product information from this URL" };
+    }
+
+    const newPrice = parseFloat(productData.currentPrice);
+    const currency = productData.currencyCode || "USD";
+
+    const { data: existingProduct } = await supabase
+      .from("products")
+      .select("id", user.id)
+      .eq("user_id", user.id)
+      .eq("url", url)
+      .single();
+
+    const isUpdate = !!existingProduct;
+
+    // Upsert product (insert or update based on user_id + url)
+    const { data: product, error } = await supabase.from("products").upsert();
+  } catch (error) {}
 }
