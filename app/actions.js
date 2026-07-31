a2c3a93dcc26045062b9dbee0f12a8346275cfc8
Alavya Pandey <alavya.pandey@gmail.com>
2026-07-31 13:46:36 +0530

    complete api actions

--- app/actions.js
+++ app/actions.js
@@ -45,6 +45,103 @@ export async function addProduct(fromData) {
     const isUpdate = !!existingProduct;
 
     // Upsert product (insert or update based on user_id + url)
-    const { data: product, error } = await supabase.from("products").upsert();
-  } catch (error) {}
+    const { data: product, error } = await supabase
+      .from("products")
+      .upsert(
+        {
+          user_id: user.id,
+          url,
+          name: productData.productName,
+          currency_price: newPrice,
+          currency: currency,
+          image_url: productData.productImageUrl,
+          updated_at: new Date().toISOString(),
+        },
+        {
+          onConflict: "user_id, url", // Unique contraint on user_id +url
+          ignoreDuplicates: false, // Always update if exists
+        },
+      )
+      .select()
+      .single();
+
+    if (error) throw Error;
+
+    // Add to price history if it's a new product OR price changed
+    const shouldAddHistory =
+      !isUpdate || existingProduct.currency_price !== new newPrice();
+
+    if (shouldAddHistory) {
+      await supabase.from("price_histroy").insert({
+        product_id: product.id,
+        price: newPrice,
+        currency: currency,
+      });
+    }
+
+    revalidatePath("/");
+    return {
+      success: true,
+      product,
+      message: isUpdate
+        ? "Product updated with latest price!"
+        : "Product added successfully",
+    };
+  } catch (error) {
+    console.log("Add product error:", error);
+    return { error: error.message || "Failed to add product" };
+  }
+}
+
+export async function deleteProduct(productId) {
+  try {
+    const supabase = await createClient();
+
+    const { error } = await supabase
+      .from("products")
+      .delete()
+      .eq("id", productId);
+
+    if (error) throw error;
+
+    revalidatePath("/");
+    return { success: true };
+  } catch (error) {
+    return { error: error.message };
+  }
+}
+
+export async function getProducts() {
+  try {
+    const supabase = await createClient();
+    const { data, error } = await supabase
+      .from("products")
+      .select("*")
+      .order("created_at", { ascending: false });
+
+    if (error) throw error;
+    return data || [];
+  } catch (error) {
+    console.log("Get products error:", error);
+    return [];
+  }
+}
+
+export async function getPriceHistory(productId) {
+  try {
+    const supabase = await createClient();
+    const { data, error } = await supabase
+      .from("price_history")
+      .select("*")
+      .eq("product_id", productId)
+      .order("checked_at", { ascending: true });
+
+      if (error) throw error;
+      return data || [];
+
+
+  } catch (error) {
+    console.log("Get price history error:", error);
+    return [];
+  }
 }
